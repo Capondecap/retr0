9d09db35bad7ec2ef248e8770c4f98c03a2f0b0f
Capondecap <55050348+Capondecap@users.noreply.github.com>
2019-10-03 12:07:05 +0700

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 #clear
-
 clear
 
 #Logo and stuffs
-
-	echo " ################### "
-	echo " Wait, loading...... "
-	echo " ################### "
+	
 	figlet RETR0
+	sleep 1
+	
+#clear
+
+	clear	
 
 #Interface Names
+
 	interface=$'wlan0'
         interface1=$'wlan1'
         interface2=$'wlx7c8bca0b520e'
